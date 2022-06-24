450e9b340342f75b5b037c33d0cd3736411a7bda
Steph <stazatorres@hotmail.com>
2022-06-24 21:54:46 +0100

    started with basic calculator setup, grid for number buttons, need to add operator buttons
    using buttons as they should already have a onclick setup

---
+++ script.js
@@ -0,0 +1,4 @@
+//Calculator project
+
+/*  -Populate screen with number buttons and operator buttons, each have a different container
+     use grid for this, and/or a similar function to etch-a-sketch*/
