9a1a608781da4e931329e186e54028c3509723f5
Steph <stazatorres@hotmail.com>
2022-06-26 11:31:59 +0100

    equals button for plus and minus implemented
    splitting number string into two parts and parsing to int so can be used in different operator functions happens in splitString(), works for + and -, need to try with divide and times symbol.
    whichOperator() also checks to see which symbol is present in number, and depending will use appropriate operator function.
    eventlisteners adjusted so only one operator can be used at one time and only once.

--- script.js
+++ script.js
@@ -21,7 +21,11 @@
     
     -need to add a clear/reset button and delete/backspace
     
-    split string, before and after operator to give two different numbers*/
+    -split string, before and after operator to give two different numbers
+        -number.slice(0, indexOf(+)) would give a string upto the plus sign
+        -number.slice((indexOf(+)+1) should give the remaining string
+    
+    -once one operator is clicked, other operators disabled*/
 
     
 
@@ -32,11 +36,16 @@
     const minusButton = document.getElementById('minusButton');
     const timesButton = document.getElementById('timesButton');
     const divideButton = document.getElementById('divideButton');
+    const decimalButton = document.getElementById('decimalButton');
+    const equalsButton = document.getElementById('equalsButton');
     const numberButtonContainer = document.querySelector('.numberButtons');
+
+
     const display = document.querySelector('.screen');
     let numA;
     let numB;
-
+    let result;
+    let number;
     let numberButton;
     for (i=9; i>=0; i--){
         numberButton = document.createElement('button');
@@ -45,26 +54,95 @@
         numberButtonContainer.appendChild(numberButton);
         numberButton.addEventListener('click', function(){
             display.innerText += this.innerText;
+            number = display.innerText;
+            console.log(number);
         })
     }
-    plusButton.addEventListener('click', function(){
-        display.innerText += this.innerText;
-    })
-    minusButton.addEventListener('click', function(){
-        display.innerText += this.innerText;
-    })
-    timesButton.addEventListener('click', function(){
-        display.innerText += this.innerText;
-    })
-    divideButton.addEventListener('click', function(){
-        display.innerText += this.innerText;
+    plusButton.addEventListener('click', addOperatorToDisplayText);
+    plusButton.addEventListener('click', disableButtons);        
+    
+    minusButton.addEventListener('click', addOperatorToDisplayText);
+    minusButton.addEventListener('click', disableButtons);
+    
+    timesButton.addEventListener('click', addOperatorToDisplayText);
+    timesButton.addEventListener('click', disableButtons);
+
+    divideButton.addEventListener('click', addOperatorToDisplayText);
+    divideButton.addEventListener('click', disableButtons);
+
+    equalsButton.addEventListener('click', splitString);
+    equalsButton.addEventListener('click', whichOperator);
+
+    decimalButton.addEventListener('click', function(){
+        display.innerText += this.innerText;//this will need some work so you can only use once decimal either side of operator.
     })
     
-    function displayInput(){
+    function addOperatorToDisplayText(){
+        display.innerText += this.innerText
+    }
+
+    function disableButtons(){
+      plusButton.removeEventListener('click', addOperatorToDisplayText);
+      plusButton.classList.add('disabled');
+      minusButton.removeEventListener('click', addOperatorToDisplayText);
+      minusButton.classList.add('disabled');
+      timesButton.removeEventListener('click', addOperatorToDisplayText);
+      timesButton.classList.add('disabled');
+      divideButton.removeEventListener('click', addOperatorToDisplayText);
+      divideButton.classList.add('disabled');
+    }
+
+    function add(){
+
+        result = numA + numB
+        display.innerText = result
+        
+    }
+
+    function subtract(){
+        result = numA - numB;
+        display.innerText = result;
+    }
+
+    function divide(){
+        result = numA/numB
+        display.innerText = result;
+
+    }
+
+    function muliply(){
+        result = numA * numB;
+        display.innerText = result;
+    }
+
+    function splitString(){
+        let fisrtNumber;
+        let secondNumber;
+        if(number.includes('+')){
+            fisrtNumber = number.slice(0, (number.indexOf('+')));
+            secondNumber = number.slice((number.indexOf('+')+1));
+        } else if(number.includes('-')){
+            fisrtNumber = number.slice(0,(number.indexOf('-')));
+            secondNumber = number.slice((number.indexOf('-')+1));
+        }
+        numA = parseInt(fisrtNumber);
+        numB = parseInt(secondNumber);
+        return numA, numB;
+    }
+
+    function whichOperator(){
+        if(number.includes('+')){
+            add();
+        }else if(number.includes('-')){
+            subtract();
+        }
+    }
+
+
         
 
 
 
-    }
+    
 
     
